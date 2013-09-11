759b72fa9fa43db806eeb527607c1a9ff56c8858
Dave Parfitt <dparfitt@basho.com>
2013-09-11 13:31:26 -0400

    add color

--- tentacles.rb
+++ tentacles.rb
@@ -3,6 +3,7 @@ require 'readline'
 require 'net/ssh'
 require 'highline/import'
 require 'yaml'
+require 'colorize'
 
 config = YAML.load_file('config.yml')
 
@@ -12,7 +13,7 @@ config = YAML.load_file('config.yml')
 
 def run(cmd, servers) 
   servers.each do |s|
-    puts ">>> #{s}"
+    puts ">>> #{s}".red
     Net::SSH.start(s, @username, :forward_agent=>true) do |session|
       session.open_channel do |channel|
         channel.request_pty do |c, success|
@@ -42,6 +43,11 @@ def readline_with_hist_management
   line
 end
 
+begin
+  Readline.emacs_editing_mode
+rescue NotImplementedError
+  libedit = true
+end
 
 
 while line = readline_with_hist_management
@@ -49,11 +55,11 @@ while line = readline_with_hist_management
     @currentcluster = line[1,line.length].strip()
     puts @currentcluster
   elsif line.strip() == "cluster" then
-    puts @currentcluster
-    puts config[@currentcluster]
+    puts @currentcluster.to_s.blue
+    puts config[@currentcluster].to_s.blue
   elsif line.strip() == "config" then
     config.keys.each do |k|
-      puts "#{k}: #{config[k]}"
+      puts "#{k}: #{config[k]}".green
     end
   else
     run(line, config[@currentcluster])
